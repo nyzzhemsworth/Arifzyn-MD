6dd33251e82de1231e58f7283139bd7bf054ca51
nyzzhemsworth <156542084+nyzzhemsworth@users.noreply.github.com>
2024-03-18 14:36:41 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,21 +10,21 @@ const chalk = require("chalk");
 global.lol = ""; // https://api.lolhuman.xyz
 global.xzn = ""; // https://xnz.wtf
 /* ~~~~~~~~~ SETTINGS OWNER ~~~~~~~~~ */
-global.numberowner = "62895347198105"; // Owner Utama
-global.owner = ["62895347198105"]; // Owner Lainnya
-global.namaowner = "Arifzyn."; // Nama Owner
-global.premium = ["62895347198105"]; // Premium User
-global.nobot = "62895347198105";
+global.numberowner = "6283897159514"; // Owner Utama
+global.owner = ["6283897159514"]; // Owner Lainnya
+global.namaowner = "Nyzz."; // Nama Owner
+global.premium = ["6283897159514"]; // Premium User
+global.nobot = "6283897159514";
 /* ~~~~~~~~~ SETTINGS BOT ~~~~~~~~~ */
-global.namabot = "Arifzyn-MD"; // NickBot
+global.namabot = "NYZBOT MD"; // NickBot
 global.typemenu = "v2"; // 'v1' > 'v2' > 'v3' > 'v4'
 global.typereply = "v1"; // 'v1' > 'v2'
-global.autoread = false; // ReadChat
+global.autoread = true; // ReadChat
 global.autobio = false; // AutoBio
 global.autoblok212 = true; // AutoBlock Nomer +212
 global.onlyindo = false; // AutoBlock Selain Nomer Indo
 global.packname = "Copyright © 2023"; // Watermark Sticker
-global.author = "Arifzyn."; // Watermark Sticker
+global.author = "Nyzz Ganteng."; // Watermark Sticker
 /* ~~~~~~~~~ MESSAGES ~~~~~~~~~ */
 global.mess = {
   done: "Done ✅",
@@ -42,7 +42,7 @@ global.mess = {
 global.thumb = fs.readFileSync("./media/quoted.jpg");
 global.menu = fs.readFileSync("./media/menu.jpg");
 /* ~~~~~~~~~ EDITS LINK ~~~~~~~~~ */
-global.link = "https://chat.whatsapp.com/LfBvDxQujrLHihSRI6TCIZ";
+global.link = "https://chat.whatsapp.com/E8UDOwxlfnIJJa283a3PE4";
 /* ~~~~~~~~~ END SYSTEM ~~~~~~~~~ */
 let file = require.resolve(__filename);
 fs.watchFile(file, () => {
